4725e1ac06ca498f691ff27e226b68763eaf454d
Ovenden <oveni@L35925MEU.local>
2020-06-25 09:41:41 +0100

    June update

--- src/containers/App.js
+++ src/containers/App.js
@@ -4,6 +4,12 @@ import Persons from '../components/Persons/Persons';
 import Cockpit from '../components/Cockpit/Cockpit';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    console.log('[App.js] constructor');
+  }
+
   state = {
     persons: [
       { id: '1', name: 'Max', age: 28 },
@@ -13,11 +19,20 @@ class App extends Component {
     otherState: 'some other value'
   }
 
-deletePersonHandler = (personIndex) => {
-  const persons = [...this.state.persons];
-  persons.splice(personIndex, 1);
-  this.setState({persons: persons});
-}
+  static getDerivedStateFromProps(props, state) {
+    console.log('[App.js] getDerivedStateFromProps', props);
+    return state;
+  }
+
+  componentDidMount(){
+    console.log('[App.js] componentDidMount');
+  }
+
+  deletePersonHandler = (personIndex) => {
+    const persons = [...this.state.persons];
+    persons.splice(personIndex, 1);
+    this.setState({persons: persons});
+  }
 
   nameChangedHandler = (event, id) => {
 
@@ -44,6 +59,8 @@ deletePersonHandler = (personIndex) => {
 
   render () {
 
+    console.log('[App.js] render');
+
     let persons = null;
 
     if (this.state.showPersons) {
@@ -56,6 +73,7 @@ deletePersonHandler = (personIndex) => {
     return (
       <div className={classes.App}>
         <Cockpit 
+          title={this.props.appTitle}
           showPersons={this.state.showPersons}
           persons={this.state.persons}
           clicked={this.togglePersonsHandler}/>
